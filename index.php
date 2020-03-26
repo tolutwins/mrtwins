7ed756b37e6b2ac33befb0d6d90709f20d13e29e
tolutwins <57286829+tolutwins@users.noreply.github.com>
2020-03-26 12:43:55 +0100

    added index.php

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
 <?php
-    echo "hello";
+   include("index.html");
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Interactive Video Gallery: More JS Explanation</title>
-        <link href="css/more.css" rel="stylesheet">
-    </head>
-    <body>
-        <div class="container">
-            <div class="displayContainer">
-                <input type="text" class="display" value="" />
-            </div>
-            <div class="numbersContainer">
-                
-            </div>
-        </div>
-
-        <!-- <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-            crossorigin="anonymous"></script> -->
-        <script src="js/more.js"></script>
-    </body>
-</html>
